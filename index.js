8a36473a02351be779689bfbf1e5e3ebbfac6f04
Nagy Attila Gabor <mrbig@sneaker.hu>
2016-04-01 22:40:44 +0200

    Added unibabel to handle utf8 strings

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/unibabel-js/index.js', function(req, res){
+  res.sendFile(__dirname + '/unibabel-js/index.js');
+});
+
+app.get('/unibabel-js/unibabel.hex.js', function(req, res){
+  res.sendFile(__dirname + '/unibabel-js/unibabel.hex.js');
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
